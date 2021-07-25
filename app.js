3cfcc32999cdc9b0db8787bebc87f56794d8b40c
nomanabir12 <alnomanofficial@gmail.com>
2021-07-25 21:05:23 +0600

    Fuction running

--- app.js
+++ app.js
@@ -1,68 +1,116 @@
-console.time('my code took');
-console.log('hello');
-console.log('abir', 'noman');
-console.log(4+32);
-console.log(true);
-console.log([2,45,6,12]);
-// js object
-console.log({abir: 'this', marks:32});
-// js object table
-console.table({abir: 'this', marks:32});
-// js warning alert
-console.warn('this is a warning for you');
-// All clear
-// console.clear()
-console.timeEnd('my code took');
-console.assert(200<150, 'Age > 150 is not possible');
-console.error('this is an error');
+// console.time('my code took');
+// console.log('hello');
+// console.log('abir', 'noman');
+// console.log(4+32);
+// console.log(true);
+// console.log([2,45,6,12]);
+// // js object
+// console.log({abir: 'this', marks:32});
+// // js object table
+// console.table({abir: 'this', marks:32});
+// // js warning alert
+// console.warn('this is a warning for you');
+// // All clear
+// // console.clear()
+// console.timeEnd('my code took');
+// console.assert(200<150, 'Age > 150 is not possible');
+// console.error('this is an error');
 
-// VARIABLES
+// // VARIABLES
 
-var name = 'abir';
-console.log(name);
-var channel;
-channel = 'js'
-console.log(name,channel)
+// var name = 'abir';
+// console.log(name);
+// var channel;
+// channel = 'js'
+// console.log(name,channel)
 
-var area = 'dhaka';
-console.log(area);
+// var area = 'dhaka';
+// console.log(area);
 
-const ownersName = 'javaScript';
-// ownersName = 'python'; const name not change
-console.log(ownersName);
+// const ownersName = 'javaScript';
+// // ownersName = 'python'; const name not change
+// console.log(ownersName);
 
-{
-let area = 'Feni';
-area = 'ctg';
-console.log(area);
-}
-console.log(area);
-
-const arr1 = [12,32,41,56,1,4];
-arr1.push(99);
-console.log(arr1); 
-
-{
-    let x = 1, y = 3;
-    alert(y-x);
-}
-{
-let apples = "2";
-let oranges = "3";
-
-// both values converted to numbers before the binary plus
-alert( +apples + +oranges );
-}
 // {
-// let a = 1;
-// let b = 2;
+// let area = 'Feni';
+// area = 'ctg';
+// console.log(area);
+// }
+// console.log(area);
 
-// let c = 3 - (a = b + 1);
-// alert(a);
-// alert(c);
+// const arr1 = [12,32,41,56,1,4];
+// arr1.push(99);
+// console.log(arr1); 
+
+// {
+//     let x = 1, y = 3;
+//     alert(y-x);
 // }
 // {
-// let counter = 2;
-// counter++;        // works the same as counter = counter + 1, but is shorter
-// alert( counter );
+// let apples = "2";
+// let oranges = "3";
+
+// // both values converted to numbers before the binary plus
+// alert( +apples + +oranges );
 // }
+// // {
+// // let a = 1;
+// // let b = 2;
+
+// // let c = 3 - (a = b + 1);
+// // alert(a);
+// // alert(c);
+// // }
+// // {
+// // let counter = 2;
+// // counter++;        // works the same as counter = counter + 1, but is shorter
+// // alert( counter );
+// // }
+// alert( false == 0 );
+
+// {
+// let myVar;
+// myVar = String(34);
+// console.log(myVar, (typeof myVar));
+// }
+// {
+// let booleanVar = true;
+// console.log(booleanVar, (typeof booleanVar));
+// }
+// {
+// let date = new Date();
+// console.log(date, (typeof date));
+// }
+// Let str = Number("2525");
+// str = Number("2525");
+// console.log(str, (typeof str));
+
+// let i = 0;
+// while (i < 3) { 
+//   alert( i );
+//   i++;
+// }
+
+// let number = parseInt('34.8778');
+// console.log(number, (typeof number));
+
+// Function
+let name = 'abir';
+let name2 = 'mahmud'
+console.log(`Welcome to our website ${name}`);
+console.log(`Welcome to our website ${name2}`);
+
+let userName = 'abir';
+
+function showMessage() {
+  userName = "abir"; 
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); 
+
+showMessage();
+
+alert( userName );
